db76d336559940a1fb8b28c9375b10f6470c03a8
DELL <DELL@DESKTOP-EBJGPVB>
2022-01-28 00:01:43 +0300

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import React from 'react';
-import Search from './components/Search';
+import React, { useState } from 'react';
+import Dropdown from './components/Dropdown';
+//import Search from './components/Search';
 //import Accordion from './components/Accordion';
 //import Counter from './components/Counter';
 
-const items = [
+//const items = [
+//    {
+//        title: 'What is react',
+//        content: 'React is a front-end javascript network'
+//    },
+//    {
+//        title: 'Why use React?',
+//        content: 'React is a favorite JS library among engineers'
+//    },
+//    {
+//        title: 'How do you use React?',
+//        content: 'You use React by creating components'
+//    }
+//]
+
+const options = [
     {
-        title: 'What is react',
-        content: 'React is a front-end javascript network'
+        label: 'The Color Red',
+        value: 'red'
     },
     {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
+        label: 'The Color Green',
+        value: 'green'
     },
     {
-        title: 'How do you use React?',
-        content: 'You use React by creating components'
+        label: 'A Shade of Blue',
+        value: 'blue'
     }
-]
+];
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Search />
-            </div>
-        );
-    };
-}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+    const [selected, setSelected] = useState(options[0]);
 
-export default App;
+    return (
+        <div>
+            <Dropdown 
+                selected={selected} 
+                onSelectedChange={setSelected}
+                options={options} 
+            />
+        </div>
+    );
+};
