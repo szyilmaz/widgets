ead0932818c319fb3a732105b40fae49bd6a001c
szyilmaz <szyilmaz@yukselproje.com.tr>
2022-01-31 14:20:23 +0300

    router added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,64 @@
 import React, { useState } from 'react';
 import Dropdown from './components/Dropdown';
-//import Search from './components/Search';
-//import Accordion from './components/Accordion';
-//import Counter from './components/Counter';
+import Search from './components/Search';
+import Accordion from './components/Accordion';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
-//const items = [
-//    {
-//        title: 'What is react',
-//        content: 'React is a front-end javascript network'
-//    },
-//    {
-//        title: 'Why use React?',
-//        content: 'React is a favorite JS library among engineers'
-//    },
-//    {
-//        title: 'How do you use React?',
-//        content: 'You use React by creating components'
-//    }
-//]
+const items = [
+    {
+        title: 'What is react',
+        content: 'React is a front-end javascript network'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
 
-//const options = [
-//    {
-//        label: 'The Color Red',
-//        value: 'red'
-//    },
-//    {
-//        label: 'The Color Green',
-//        value: 'green'
-//    },
-//    {
-//        label: 'A Shade of Blue',
-//        value: 'blue'
-//    }
-//];
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    }
+];
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-           <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a color"
+                    options={options} 
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
